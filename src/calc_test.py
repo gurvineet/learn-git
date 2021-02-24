b2c0e02636b16c44603b0b4b2434e949fba53361
gurvineet <44764396+gurvineet@users.noreply.github.com>
2021-02-24 11:11:11 -0800

    Revert "Gurvineet/added comment (#1)"
    This reverts commit 3f6a92cb5c09d37f25c7e1aa357b8b3fa3b0411b.

--- src/calc_test.py
+++ src/calc_test.py
@@ -8,12 +8,8 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(add(3, 4), 7, 'adding three and four')
 
     def test_sub_2arg(self):
-        # Make sure 4 - 3 = 1. Test
+        # Make sure 4 - 3 = 1
         self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
-
-    def test_sub_3arg(self):
-        # Make sure 4 - 3 = 1. Test
-        self.assertEqual(sub(4, 3, 1), 0, 'subtracting one from three from four')
 
 
 if __name__ == '__main__':
